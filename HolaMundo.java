ae061c1a80b5fff801cde81c33608eaf91871995
JMMB-360 <jmanbre360@iesalmudeyne.es>
2023-11-10 10:57:54 +0100

    Primer commit de HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,14 @@
+/*
+ * Muestra por pantalla la frase "¡Hola mundo!"
+*/
+public class HolaMundo {
+    public static void main (String[] args) {
+        System.out.println("¡Hola mundo!");
+    }
+}
+
+// git tag -a VERSION -m "DESCRIPCIÓN"
+// Etiqueta el cambio realizado como una nueva versión del proyecto
+
+// git tag -n
+// Muestra todas las versiones
