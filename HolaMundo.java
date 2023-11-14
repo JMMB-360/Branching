c15866b4eaa280b3a3ad57959defc72d4ef463ab
JMMB-360 <jmanbre360@iesalmudeyne.es>
2023-11-14 14:18:17 +0100

    Uso de los colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,8 @@ public class HolaMundo {
     public static void main (String[] args) {
         String rojo = "\033[32m";
         String azul = "\033[34m";
-        System.out.println("¡Hola mundo!");
+        System.out.println(rojo +"¡Hola mundo!");
+        System.out.println(azul +"Aquí estamos haciendo pruebas en Git");
     }
 }
 
