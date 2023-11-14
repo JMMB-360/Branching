9b1a7a4d78932bb3a64c6f748e801e75f46ce262
JMMB-360 <jmanbre360@iesalmudeyne.es>
2023-11-14 14:04:39 +0100

    HolaMundo con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@
 */
 public class HolaMundo {
     public static void main (String[] args) {
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("¡Hola mundo!");
     }
 }
@@ -12,3 +14,9 @@ public class HolaMundo {
 
 // git tag -n
 // Muestra todas las versiones
+
+// git branch "nombre_rama"
+// Crea una nueva rama (con -v vemos las ramas actuales)
+
+// git checkout "nombre_rama"
+// Cambia a la rama indicada (con -b la crea y se mueve a ella)
